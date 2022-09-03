5c42476a7d49de4b3fa80433495e554e23fe1a56
eshan-003 <107420267+eshan-003@users.noreply.github.com>
2022-09-04 00:43:04 +0530

    Update Login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -56,6 +56,7 @@ const Login = () => {
       });
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
+        window.location.reload(false);
       history.push("/chats");
     } catch (error) {
       toast({
